f29a710415d6ef9840b5deb71e2ec0931a06fbfe
Doug <doug@anetsc.com>
2021-03-25 16:06:15 -0700

    Replace ParseJSON Logic with bson.UnmarshalExtJSON

--- jsonparser.go
+++ jsonparser.go
@@ -6,9 +6,7 @@ package mongolang
 */
 
 import (
-	"encoding/json"
 	"fmt"
-	"strings"
 
 	"go.mongodb.org/mongo-driver/bson"
 )
@@ -16,14 +14,15 @@ import (
 // JSONToBSON supports parsing JSON strings
 // into BSON structures, either bson.D or bson.A
 type JSONToBSON struct {
-	decoder *json.Decoder
-	token   json.Token
-
 	IsBSOND bool
-	Err     error
+	IsBSONA bool
+	IsOther bool
+
+	Err error
 
 	BSOND bson.D
 	BSONA bson.A
+	Other interface{}
 }
 
 // LastJSONToBSON contains the last instance of this struct
@@ -45,172 +44,50 @@ func ParseJSONToBSON(jsonStr string) interface{} {
 
 	if err != nil {
 		fmt.Printf("error in ParseJSONToBSON: %v \n", err)
-		return err
+		return nil
 	}
 	return result
 }
 
-// ParseJSON parses JSON strings
-// to create a bson.D or bson.A struct
+// ParseJSON parses a JSON string
+// to create a bson.D, bson.A or "other" structure.
+// Uses the MongoDB canonical (strict) extended JSON format documented at
+// https://docs.mongodb.com/manual/reference/mongodb-extended-json/
+//
+// For example, a filter string searching for specific _id would be coded as
+//
+//    filter := `{"_id" : {"$oid":"5bf36072a5820f6e28a4736c"} }`
+//
+// Note use of backticks, NOT single quotes, to create a Go "raw string" which
+// can contain unescaped quote marks.
+//
+// See  https://docs.mongodb.com/manual/reference/mongodb-extended-json/ for
+// a complete list of extensions along with examples.
+//
+// In addition to returning a bson or other struct, ParseJSON sets a series of flags
+// to define if the result was a bson.A, bson.D or "other" type. It also sets the
+// corresponding type value, BSOND, BSONA, or Other to pointers to the results.
 func (j *JSONToBSON) ParseJSON(jsonStr string) (interface{}, error) {
 
-	j.decoder = json.NewDecoder(strings.NewReader(jsonStr))
-	j.token, j.Err = j.decoder.Token()
+	var doc interface{}
+	j.Err = bson.UnmarshalExtJSON([]byte(jsonStr), true, &doc)
 
 	if j.Err != nil {
 		return nil, j.Err
 	}
 
-	switch v := j.token.(type) {
-	case json.Delim:
-		if v.String() == "{" {
-			j.IsBSOND = true
-			j.BSOND = bson.D{}
-
-			var bsonD bson.D
-			bsonD, j.Err = j.parseNameValue()
-
-			if j.Err == nil {
-				j.BSOND = bsonD
-			}
-		} else {
-			j.IsBSOND = false
-			j.BSONA, j.Err = j.parseArray()
-
-		}
+	switch v := doc.(type) {
+	case bson.D:
+		j.BSOND = v
+		j.IsBSOND = true
+	case bson.A:
+		j.BSONA = v
+		j.IsBSONA = true
 	default:
-		j.Err = fmt.Errorf("unrecognized or invalid type %T value %v", j.token, j.token)
-		return nil, j.Err
-	}
-
-	if j.IsBSOND {
-		return j.BSOND, j.Err
-	}
-
-	return j.BSONA, j.Err
-
-}
-
-// parse a bson.E name value pair
-func (j *JSONToBSON) parseNameValue() (bson.D, error) {
-	result := bson.D{}
-
-	for {
-		j.token, j.Err = j.decoder.Token()
-		if j.Err != nil {
-			return bson.D{}, j.Err
-		}
-
-		switch v := j.token.(type) {
-		case string:
-			var value interface{}
-			value, j.Err = j.parseValue()
-			element := bson.E{Key: string(v), Value: value}
-			result = append(result, element)
-		case json.Delim:
-			delim := string(v)
-			if delim == "{" {
-				fmt.Println("found '{' in parseNameValue()")
-				var value interface{}
-				value, j.Err = j.parseValue()
-				element := bson.E{Key: string(v), Value: value}
-				result = append(result, element)
-			} else if delim == "}" {
-				return result, j.Err
-			} else {
-				// didn't find the expected "}" delimiter
-				j.Err = fmt.Errorf("looking key:value pair, found type %T value %v", j.token, j.token)
-				return result, j.Err
-			}
-		default:
-			j.Err = fmt.Errorf("looking key:value pair, found type %T value %v", j.token, j.token)
-			return result, j.Err
-		}
-	}
-	// return result, j.Err
-}
-
-// parse a json value.
-// Either a string, float64, bool, nil, bson.E or bson.A
-func (j *JSONToBSON) parseValue() (interface{}, error) {
-	j.token, j.Err = j.decoder.Token()
-	if j.Err != nil {
-		return j.token, j.Err
-	}
-
-	switch v := j.token.(type) {
-	case string:
-		return string(v), nil
-	case float64:
-		return float64(v), nil
-	case bool:
-		return bool(v), nil
-	case nil:
-		return nil, nil
-	case json.Delim:
-		delim := string(v)
-		if delim == "[" {
-			return j.parseArray()
-		}
-		if delim == "{" {
-			return j.parseNameValue()
-		}
+		j.Other = v
+		j.IsOther = true
 
 	}
 
-	j.Err = fmt.Errorf("looking for a json value or bson.E or bson.A, found %T value %v", j.token, j.token)
-	return nil, j.Err
-}
-
-// parse an array of values
-func (j *JSONToBSON) parseArray() (bson.A, error) {
-	result := bson.A{}
-
-	for {
-		j.token, j.Err = j.decoder.Token()
-		if j.Err != nil {
-			return result, j.Err
-		}
-
-		switch v := j.token.(type) {
-		case string:
-			result = append(result, string(v))
-		case float64:
-			result = append(result, float64(v))
-		case bool:
-			result = append(result, bool(v))
-		case nil:
-			result = append(result, nil)
-
-		case json.Delim:
-			delim := string(v)
-			if delim == "[" {
-				var element bson.A
-				element, j.Err = j.parseArray()
-				if j.Err != nil {
-					return result, j.Err
-				}
-
-				result = append(result, element)
-
-			} else if delim == "{" {
-				var element bson.D
-				element, j.Err = j.parseNameValue()
-				if j.Err != nil {
-					return result, j.Err
-				}
-				result = append(result, element)
-
-			} else if delim == "]" {
-				return result, j.Err
-			}
-
-			// we assume json.Decoder will balance proper {} or []
-			// therefore, we have an ending delimiter
-			// return the results.
-
-			// return result, j.Err
-		}
-
-	}
+	return doc, nil
 }
