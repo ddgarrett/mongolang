2bda93bd5626530d1f0788c7a855aae1c4af29dc
Doug <doug@anetsc.com>
2021-03-25 10:26:51 -0700

    Add Version # to hello.go Message

--- hello.go
+++ hello.go
@@ -8,6 +8,6 @@ import (
 // Hello returns a greeting for the named person.
 func Hello(name string) string {
 	// Return a greeting that embeds the name in a message.
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf("Hi, %v. Welcome to version 0.2.2 of MonGolang!", name)
 	return message
 }
