35266c884453b674b4f9b0897671d1c2dde647e8
Doug <doug@anetsc.com>
2021-05-05 08:27:53 -0700

    Add Error Tests for Cursor

--- cursor_test.go
+++ cursor_test.go
@@ -3,6 +3,8 @@ package mongolang
 import (
 	"strings"
 	"testing"
+
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 func TestSkip(t *testing.T) {
@@ -50,5 +52,69 @@ func TestString(t *testing.T) {
 
 	if i < 0 {
 		t.Errorf("TestSkip didn't find \"Value\": \"NORWALK\" in \n%s", result)
+	}
+}
+
+// TestErr tests various cursor error conditions
+func TestCursorErr(t *testing.T) {
+
+	cursor := &Cursor{}
+	if cursor.Err() == nil {
+		t.Error("Err() did not return invalid cursor error")
+	}
+
+	if cursor.requireOpenCursor() {
+		t.Error("requireOpenCursor() returned true for invalid cursor")
+	}
+
+	db := DB{}
+	db.InitMonGolang("mongodb://localhost:27017")
+	defer db.Disconnect()
+
+	// Make sure error returned is not connected to db
+	cursor = db.Coll("zips").Find().Limit(1)
+	if cursor.Err() != ErrNotConnectedDB {
+		t.Errorf("expected ErrNotConnectedDB, got %v", cursor.Err())
+	}
+
+	// Test closed cursor
+	db.Use("quickstart")
+	cursor = db.Coll("zips").Find(`{"state":"CA"}`)
+	cursor.HasNext() // make sure we opened a  Mongo cursor
+	cursor.Close()
+
+	// try to close twice
+	e := cursor.Close()
+	if e != ErrClosedCursor {
+		t.Errorf("expected ErrClosedCursor from Close() call got %v", e)
+	}
+
+	// other calls on closed cursor
+	cursor.Sort(`{"zip":1}`).HasNext()
+	cursor.Pretty()
+	cursor.ToArray()
+	cursor.Next()
+	cursor.Count()
+	s := cursor.String()
+	if cursor.Err() != ErrClosedCursor {
+		t.Errorf("expected ErrClosedCursor got %v, String(): %s", cursor.Err(), s)
+	}
+
+	// invalid sort parms
+	db.Coll("zips").Find(`{"state":"CA"}`).Sort(bson.A{})
+	if db.Err == nil {
+		t.Error("expected error from invalid sort parm")
+	}
+
+	// test HasNext() with buffered doc
+	cursor = db.Coll("zips").Find(`{"state":"CA"}`)
+	cursor.HasNext()
+	cursor.HasNext()
+
+	// next with no next available
+	cursor = db.Coll("zips").Find(`{"state":"abc"}`)
+	cursor.Next()
+	if cursor.Err() == nil {
+		t.Error("expected error from invalid Next() call")
 	}
 }
